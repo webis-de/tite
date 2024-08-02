a404e3389b96fb73e656bb9c45577e33ad2d9d7b
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2024-08-02 10:04:08 +0200

    remove train only mask from mlm teacher

--- tite/jepa.py
+++ tite/jepa.py
@@ -40,9 +40,7 @@ class JEPA(Module):
 
     def forward(self, input: dict, target: dict, aux: Any | None = None):
         embx = self._student(**input)
-        emby = self._teacher(
-            **{f"student_{k}": v for k, v in input.items()}, **{f"teacher_{k}": v for k, v in target.items()}
-        )
+        emby = self._teacher(**target)
         pred = self._predictor(embx, aux)
         if not self._return_embeddings:
             return self._loss(pred, emby)
--- tite/teacher.py
+++ tite/teacher.py
@@ -6,20 +6,15 @@ class Identity(Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, student_input_ids: Tensor, **kwargs) -> Tensor:
-        return student_input_ids
+    def forward(self, input_ids: Tensor, **kwargs) -> Tensor:
+        return input_ids
 
 
 class MLMPredictor(Module):
-    def __init__(self, padid: int, maskid: int, train_only_masked: bool = True) -> None:
+    def __init__(self, padid: int) -> None:
         super().__init__()
         self._pad_id = padid
-        self._mask_id = maskid
-        self.train_only_masked = train_only_masked
 
-    def forward(self, student_input_ids: Tensor, teacher_input_ids: Tensor, **kwargs) -> Tensor:
-        if self.train_only_masked:
-            teacher_input_ids = teacher_input_ids.masked_fill(student_input_ids != self._mask_id, -100)
-        else:
-            targets = teacher_input_ids.masked_fill(teacher_input_ids == self._pad_id, -100)
+    def forward(self, input_ids: Tensor, **kwargs) -> Tensor:
+        targets = input_ids.masked_fill(input_ids == self._pad_id, -100)
         return targets
