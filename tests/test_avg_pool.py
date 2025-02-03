a25bb369323fec26e9ddc41f16311d0ee2777908
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-02-03 09:28:15 +0100

    fix packed triton kernel

---
+++ tests/test_avg_pool.py
@@ -0,0 +1,116 @@
+from typing import Tuple
+
+import pytest
+import torch
+
+from tite.model import compute_output_shapes
+from tite.pool import PackedAvgPool1d, PackedMetaData
+
+
+class ReferenceMaskedAvgPool1d(torch.nn.Module):
+    def __init__(self, kernel_size: int, stride: int):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        if x.shape[-2] == 1:
+            return x, mask
+
+        batch_size, _, dim = x.shape
+
+        seq_lens = mask.sum(-1)
+        if self.kernel_size > x.shape[-2]:
+            padding = self.kernel_size - x.shape[-2]
+        else:
+            padding = (self.kernel_size - seq_lens - self.stride) % self.stride
+
+        new_seq_lens = seq_lens + padding
+        pad_x = torch.zeros(batch_size, int(new_seq_lens.max().item()), dim, device=x.device, dtype=x.dtype)
+        pad_mask = torch.zeros(batch_size, int(new_seq_lens.max().item()), device=mask.device, dtype=mask.dtype)
+        for batch_idx in range(batch_size):
+            seq_len = seq_lens[batch_idx]
+            new_seq_len = new_seq_lens[batch_idx]
+            pad_x[batch_idx, :seq_len] = x[batch_idx, :seq_len]
+            pad_mask[batch_idx, :new_seq_len] = True
+
+        x_blocks = pad_x.unfold(-2, self.kernel_size, self.stride)
+        mask_blocks = pad_mask.unfold(-1, self.kernel_size, self.stride).unsqueeze(-2)
+        y = x_blocks.mean(-1)
+        mask_blocks[:, 0, 0, :] = 1
+        y_mask = mask_blocks.amin(-1).squeeze(-1)
+        y = y.masked_fill(~y_mask[..., None].expand_as(y), 0)
+        return y, y_mask
+
+
+@pytest.mark.parametrize("kernel_size, stride, seq_length", [(3, 1, 8), (3, 2, 8), (3, 3, 8)])
+def test_masked_avg_pool1d_dimensions(kernel_size: int, stride: int, seq_length: int):
+    layer = ReferenceMaskedAvgPool1d(kernel_size, stride)
+
+    x = torch.randn(2, seq_length, 4)
+    mask = torch.ones(2, seq_length, dtype=torch.bool)
+    mask[0, -seq_length // 2 :] = False
+
+    output_shapes = compute_output_shapes(seq_length, (kernel_size,), (stride,))
+
+    output, output_mask = layer(x, mask)
+    assert output.shape[1] == output_shapes[-1]
+    assert ((output != 0).all(-1) == output_mask).all()
+    assert output_mask.shape[1] == output_shapes[-1]
+
+
+def test_masked_avg_pool1d_2_5_4_3_3():
+    layer = ReferenceMaskedAvgPool1d(3, 3)
+    x = torch.arange(2 * 5 * 4, dtype=torch.float32).reshape(2, 5, 4)
+    mask = torch.tensor([[True, True, True, True, True], [True, True, True, False, False]])
+    out, out_mask = layer(x, mask)
+    assert torch.allclose(
+        out,
+        torch.tensor(
+            [[[4.0, 5.0, 6.0, 7.0], [14.0, 15.0, 16.0, 17.0]], [[24.0, 25.0, 26.0, 27.0], [0.0, 0.0, 0.0, 0.0]]]
+        ),
+    )
+    assert torch.equal(out_mask, torch.tensor([[True, True], [True, False]]))
+
+
+def test_masked_avg_pool1d_2_5_4_8_1():
+    layer = ReferenceMaskedAvgPool1d(8, 1)
+    x = torch.arange(2 * 5 * 4, dtype=torch.float32).reshape(2, 5, 4)
+    mask = torch.tensor([[True, True, True, True, True], [True, True, True, False, False]])
+    out, out_mask = layer(x, mask)
+    assert torch.allclose(out, torch.tensor([[[8.0, 9.0, 10.0, 11.0]], [[24.0, 25.0, 26.0, 27.0]]]))
+    assert torch.equal(out_mask, torch.tensor([[True], [True]]))
+
+
+@pytest.mark.parametrize("kernel_size", range(1, 5))
+@pytest.mark.parametrize("stride", range(1, 5))
+@pytest.mark.parametrize("seq_length", [1, 2, 3, 4, 6, 8, 16, 32, 64])
+@pytest.mark.parametrize("k", [1, 2, 3, 4, 6, 8, 16, 32, 64])
+@pytest.mark.parametrize(
+    "dtype", [torch.float32, torch.float16, torch.bfloat16], ids=["float32", "float16", "bfloat16"]
+)
+def test_packed_avg_pool1d(kernel_size: int, stride: int, seq_length: int, k: int, dtype: torch.dtype):
+    masked = ReferenceMaskedAvgPool1d(kernel_size, stride)
+    packed = PackedAvgPool1d(kernel_size, stride)
+
+    x = torch.randn(2, seq_length, k, requires_grad=True, device="cuda", dtype=dtype)
+    mask = torch.ones(2, seq_length, dtype=torch.bool, device="cuda")
+    mask[0, min(-1, -seq_length // 2) :] = False
+    seq_lens = mask.sum(-1)
+    cu_seq_lens = torch.zeros(mask.shape[0] + 1, dtype=torch.int64, device="cuda")
+    cu_seq_lens[1:] = seq_lens.cumsum(0)
+
+    idcs = mask.nonzero(as_tuple=True)
+    packed_x = x[idcs].detach().clone().requires_grad_(True)
+
+    meta_data = PackedMetaData(seq_lens, cu_seq_lens, int(seq_lens.max().int()))
+
+    output_masked, out_mask = masked(x, mask)
+    output_packed, _ = packed(packed_x, meta_data)
+
+    (output_masked * torch.arange(1, x.shape[-1] + 1, device=x.device, dtype=x.dtype)).sum().backward()
+    (output_packed * torch.arange(1, x.shape[-1] + 1, device=x.device, dtype=x.dtype)).sum().backward()
+
+    assert torch.allclose(output_masked[out_mask], output_packed, atol=1e-6)
+    assert x.grad is not None and packed_x.grad is not None
+    assert torch.allclose(x.grad[mask], packed_x.grad, atol=1e-6)
--- tite/pool.py
+++ tite/pool.py
@@ -42,7 +42,8 @@ class PackedMetaData:
 
 @triton.jit
 def forward_pooling_kernel(
-    X,  # Pointers to matrices
+    # Pointers to matrices
+    X,
     Y,
     X_CU_SEQ_LENS,
     Y_CU_SEQ_LENS,
@@ -126,24 +127,36 @@ def apply_forward_pooling(
 
 
 @triton.jit
+def div_fl(a, b):
+    # division with floor
+    # a // b does not work for negative numbers in Triton
+    # (it rounds upwards for negative numbers)
+    return (a - (b - 1)) // b
+
+
+@triton.jit
 def backward_pooling_kernel(
-    Y,  # Pointers to matrices
+    # Pointers to matrices
     X,
-    Y_CU_SEQ_LENS,
+    Y,
     X_CU_SEQ_LENS,
+    Y_CU_SEQ_LENS,
     # Matrix dimensions
     dim,
     # strides
-    stride_y_seq_len,
-    stride_y_dim,
     stride_x_seq_len,
     stride_x_dim,
+    stride_y_seq_len,
+    stride_y_dim,
     # Meta-parameters
     BLOCK_K: tl.constexpr,
     BLOCK_M: tl.constexpr,
     kernel_size: tl.constexpr,
     stride: tl.constexpr,
 ):
+    # the backward kernel indexes the gradient tensor as a dilated tensor
+    # see the following article for details
+    # https://medium.com/@mayank.utexas/backpropagation-for-convolution-with-strides-8137e4fc2710
     pid_m = tl.program_id(axis=0)
     pid_batch = tl.program_id(axis=1)
     pid_window = tl.program_id(axis=2)
@@ -153,9 +166,12 @@ def backward_pooling_kernel(
     x_seq_len = tl.load(X_CU_SEQ_LENS + pid_batch + 1) - X_start_idx
     y_seq_len = tl.load(Y_CU_SEQ_LENS + pid_batch + 1) - Y_start_idx
     X = X + X_start_idx * stride_x_seq_len + pid_window * stride_x_dim
-    Y = Y + Y_start_idx * stride_y_seq_len + tl.cdiv(pid_window, stride) * stride_y_dim
+    Y = Y + Y_start_idx * stride_y_seq_len + pid_window * stride_y_dim
 
-    if pid_m * BLOCK_M >= x_seq_len:
+    padding = kernel_size - 1
+    dilation = stride - 1
+
+    if pid_m * BLOCK_M >= y_seq_len:
         return
     rm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     rk = tl.arange(0, BLOCK_K)
@@ -163,15 +179,18 @@ def backward_pooling_kernel(
     acc = tl.zeros((BLOCK_M, BLOCK_K), dtype=tl.float32)
 
     for i in range(kernel_size):
-        rm_ = tl.cdiv(rm[:, None] - i, stride)
-        Y_ = Y + (rm_ * stride_y_seq_len + rk[None, :] * stride_y_dim)
-        y = tl.load(Y_, mask=(rm_ >= 0) & (rm_ < y_seq_len) & (rk[None, :] < dim), other=0.0)
-        acc += y
+        rm_ = rm[:, None] + i - padding
+        dilation_mask = (rm_ % stride) == 0
+        padding_mask = (rm_ >= 0) & (rm_ < x_seq_len + (x_seq_len - 1) * dilation)
+        rm_ = div_fl(rm_, stride)
+        X_ = X + (rm_ * stride_x_seq_len + rk[None, :] * stride_x_dim)
+        x = tl.load(X_, mask=padding_mask & dilation_mask & (rk[None, :] < dim), other=0.0)
+        acc += x
 
     acc /= kernel_size
 
-    X = X + rm[:, None] * stride_x_seq_len + rk[None, :] * stride_x_dim
-    tl.store(X, acc, mask=(rm[:, None] < x_seq_len) & (rk[None, :] < dim))
+    Y = Y + rm[:, None] * stride_y_seq_len + rk[None, :] * stride_y_dim
+    tl.store(Y, acc, mask=(rm[:, None] < y_seq_len) & (rk[None, :] < dim))
 
 
 def apply_backward_pooling(
@@ -179,7 +198,7 @@ def apply_backward_pooling(
     y: torch.Tensor,
     x_cu_seq_lens: torch.Tensor,
     y_cu_seq_lens: torch.Tensor,
-    x_max_seq_len: int,
+    y_max_seq_len: int,
     kernel_size: int,
     stride: int,
 ) -> None:
@@ -188,7 +207,7 @@ def apply_backward_pooling(
 
     BLOCK_M = 8 if dim <= 128 else 4
     BLOCK_K = 32 if dim <= 32 else (64 if dim <= 64 else (128 if dim <= 128 else 256))
-    grid = lambda META: (triton.cdiv(x_max_seq_len, META["BLOCK_M"]), batch)  # noqa
+    grid = lambda META: (triton.cdiv(y_max_seq_len, META["BLOCK_M"]), batch)  # noqa
 
     # Need this, otherwise Triton tries to launch from cuda:0 and we get
     # ValueError: Pointer argument (at 0) cannot be accessed from Triton (cpu tensor?)
@@ -225,13 +244,13 @@ class ApplyPooling_(torch.autograd.Function):
         y = torch.zeros(y_cu_seq_lens[-1], dim, device=x.device, dtype=x.dtype)
 
         apply_forward_pooling(
-            x,
-            y,
-            packed_meta_data.cu_seq_lens,
-            y_cu_seq_lens,
-            y_max_seq_len,
-            kernel_size,
-            stride,
+            x=x,
+            y=y,
+            x_cu_seq_lens=packed_meta_data.cu_seq_lens,
+            y_cu_seq_lens=y_cu_seq_lens,
+            y_max_seq_len=y_max_seq_len,
+            kernel_size=kernel_size,
+            stride=stride,
         )
         ctx.save_for_backward(x, packed_meta_data.cu_seq_lens, y_packed_meta_data.cu_seq_lens)
         ctx.kernel_size = kernel_size
@@ -245,16 +264,15 @@ class ApplyPooling_(torch.autograd.Function):
         x_max_seq_len = ctx.x_max_seq_len
         kernel_size = ctx.kernel_size
         stride = ctx.stride
-        grad_x = None
         grad_x = torch.zeros_like(x)
         apply_backward_pooling(
-            grad_y,
-            grad_x,
-            y_cu_seq_lens,
-            x_cu_seq_lens,
-            x_max_seq_len,
-            kernel_size,
-            stride,
+            x=grad_y,
+            y=grad_x,
+            x_cu_seq_lens=y_cu_seq_lens,
+            y_cu_seq_lens=x_cu_seq_lens,
+            y_max_seq_len=x_max_seq_len,
+            kernel_size=kernel_size,
+            stride=stride,
         )
         return grad_x, None, None, None
 
@@ -267,4 +285,6 @@ class PackedAvgPool1d(torch.nn.Module):
         self.stride = stride
 
     def forward(self, x: torch.Tensor, packed_meta_data: PackedMetaData) -> Tuple[torch.Tensor, PackedMetaData]:
+        if packed_meta_data.max_seq_len == 1:
+            return x, packed_meta_data
         return ApplyPooling_.apply(x, packed_meta_data, self.kernel_size, self.stride)
