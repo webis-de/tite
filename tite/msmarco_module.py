fce1cf98bb8040dbfb1d6c5101fc61b59abc0d30
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-02-14 12:26:18 +0100

    remove amp

--- tite/msmarco_module.py
+++ tite/msmarco_module.py
@@ -43,15 +43,14 @@ class MSMARCOModule(LightningModule):
         query_emb = self.forward(batch["encoded_query"])
         pos_doc_emb = self.forward(batch["encoded_pos_doc"])
         neg_doc_emb = self.forward(batch["encoded_neg_doc"])
-        with torch.autocast(device_type="cuda", enabled=False):
-            if self.similarity == "cosine":
-                pos_sim = torch.nn.functional.cosine_similarity(query_emb.squeeze(1), pos_doc_emb.squeeze(1))
-                neg_sim = torch.nn.functional.cosine_similarity(query_emb.squeeze(1), neg_doc_emb.squeeze(1))
-            elif self.similarity == "dot":
-                pos_sim = (query_emb @ pos_doc_emb.transpose(-1, -2)).view(-1)
-                neg_sim = (query_emb @ neg_doc_emb.transpose(-1, -2)).view(-1)
-            else:
-                raise ValueError(f"Unknown similarity: {self.similarity}")
+        if self.similarity == "cosine":
+            pos_sim = torch.nn.functional.cosine_similarity(query_emb.squeeze(1), pos_doc_emb.squeeze(1))
+            neg_sim = torch.nn.functional.cosine_similarity(query_emb.squeeze(1), neg_doc_emb.squeeze(1))
+        elif self.similarity == "dot":
+            pos_sim = (query_emb @ pos_doc_emb.transpose(-1, -2)).view(-1)
+            neg_sim = (query_emb @ neg_doc_emb.transpose(-1, -2)).view(-1)
+        else:
+            raise ValueError(f"Unknown similarity: {self.similarity}")
         margin = pos_sim - neg_sim
         loss = torch.nn.functional.binary_cross_entropy_with_logits(margin, torch.ones_like(margin))
         self.log("train_loss", loss, prog_bar=True, on_epoch=True)
