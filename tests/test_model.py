455326fd4d279563ccbd405e1c490e40c816a7f1
Ferdinand Schlatt <ferdinand.schlatt@uni-jena.de>
2025-02-14 09:07:00 +0100

    cleanup model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,18 +13,22 @@ def config() -> TiteConfig:
     config = TiteConfig(
         vocab_size=32,
         num_hidden_layers=3,
-        hidden_sizes=(4, 6, 8),
+        hidden_sizes=(4, 8, 12),
         num_attention_heads=(2, 2, 2),
         intermediate_sizes=(8, 12, 16),
         kernel_sizes=(8, 8, None),
         strides=(2, 1, None),
         max_position_embeddings=16,
         positional_embedding_type="absolute",
+        attn_implementation="eager",
+        rope_implementation="eager",
+        pooling_implementation="eager",
+        compile=False,
     )
     return config
 
 
-@pytest.mark.parametrize("positional_embedding_type", ["absolute", "ALiBi"])
+@pytest.mark.parametrize("positional_embedding_type", ["absolute", "alibi", "rotary"])
 def test_tite_model(config: TiteConfig, positional_embedding_type: str):
     pytest.MonkeyPatch().setattr(config, "positional_embedding_type", positional_embedding_type)
     model = TiteModel(config)
@@ -32,7 +36,7 @@ def test_tite_model(config: TiteConfig, positional_embedding_type: str):
     attention_mask = torch.ones_like(input_ids, dtype=torch.bool)
     attention_mask[1, -config.max_position_embeddings // 2 :] = False
     output = model(input_ids, attention_mask).last_hidden_state
-    assert output.shape == (2, 1, config.last_hidden_size)
+    assert output.shape == (2, 1, config.hidden_size)
     assert output.requires_grad
 
 
@@ -93,58 +97,73 @@ def test_same_as_legacy():
         assert torch.allclose(legacy_grads[name], param.grad, atol=1e-5)
 
 
-@pytest.mark.parametrize("attn_implementation", ["sdpa", "eager"], ids=["sdpa", "eager"])
+@pytest.mark.parametrize("attn_implementation", ["sdpa", "eager", "flash_attention_2"])
 @torch.no_grad()
 def test_same_as_bert(attn_implementation: str):
+    if not torch.cuda.is_available() and attn_implementation == "flash_attention_2":
+        pytest.skip("FlashAttention2 is only available on CUDA")
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     hidden_size = 16
     num_hidden_layers = 12
     num_attention_heads = 2
     intermediate_size = 64
-    bert = BertModel(
-        BertConfig(
-            hidden_sizes=hidden_size,
-            num_hidden_layers=num_hidden_layers,
-            num_attention_heads=num_attention_heads,
-            intermediate_sizes=intermediate_size,
-            attn_implementation=attn_implementation,
+    bert = (
+        BertModel(
+            BertConfig(
+                hidden_size=hidden_size,
+                num_hidden_layer=num_hidden_layers,
+                num_attention_heads=num_attention_heads,
+                intermediate_size=intermediate_size,
+                attn_implementation=attn_implementation if attn_implementation != "flash_attention_2" else "sdpa",
+            )
         )
-    ).eval()
+        .eval()
+        .to(device)
+    )
     bert.embeddings.token_type_embeddings.weight.zero_()
-    tite = TiteModel(
-        TiteConfig(
-            hidden_sizes=(hidden_size,) * num_hidden_layers,
-            num_hidden_layers=num_hidden_layers,
-            num_attention_heads=(num_attention_heads,) * num_hidden_layers,
-            intermediate_sizes=(intermediate_size,) * num_hidden_layers,
-            kernel_sizes=(None,) * num_hidden_layers,
-            strides=(None,) * num_hidden_layers,
-            hidden_act="gelu",
-            positional_embedding_type="absolute",
-            norm_location="post",
-            norm_type="layer",
-            attn_implementation=attn_implementation,
+    tite = (
+        TiteModel(
+            TiteConfig(
+                hidden_sizes=(hidden_size,) * num_hidden_layers,
+                num_hidden_layers=num_hidden_layers,
+                num_attention_heads=(num_attention_heads,) * num_hidden_layers,
+                intermediate_sizes=(intermediate_size,) * num_hidden_layers,
+                kernel_sizes=(None,) * num_hidden_layers,
+                strides=(None,) * num_hidden_layers,
+                hidden_act="gelu",
+                positional_embedding_type="absolute",
+                norm_location="post",
+                norm_type="layer",
+                attn_implementation=attn_implementation,
+            )
         )
-    ).eval()
+        .eval()
+        .to(device)
+    )
     config = tite.config
 
     tite.load_state_dict(tite._update_state_dict(bert.state_dict()))
 
-    input_ids = torch.randint(0, config.vocab_size, (2, config.max_position_embeddings))
+    input_ids = torch.randint(0, config.vocab_size, (2, config.max_position_embeddings), device=device)
+    attention_mask = torch.ones_like(input_ids, dtype=torch.bool, device=device)
+    attention_mask[1, -config.max_position_embeddings // 2 :] = False
 
-    attention_mask = torch.ones_like(input_ids, dtype=torch.bool)
-    # attention_mask[1, -config.max_position_embeddings // 2 :] = False
-
-    bert_output = bert(
-        input_ids, attention_mask, output_hidden_states=True, output_attentions=attn_implementation == "eager"
-    )
-    tite_output = tite(
-        input_ids, attention_mask, output_hidden_states=True, output_attentions=attn_implementation == "eager"
-    )
+    with torch.autocast(device_type="cuda"):
+        bert_output = bert(
+            input_ids, attention_mask, output_hidden_states=True, output_attentions=attn_implementation == "eager"
+        )
+        tite_output = tite(
+            input_ids, attention_mask, output_hidden_states=True, output_attentions=attn_implementation == "eager"
+        )
 
     for i in range(config.num_hidden_layers):
         if tite_output.attentions is not None:
-            assert torch.allclose(tite_output.attentions[i], bert_output.attentions[i], atol=1e-6)
-        assert torch.allclose(tite_output.hidden_states[i], bert_output.hidden_states[i][attention_mask], atol=1e-6)
+            assert torch.allclose(
+                tite_output.attentions[i][attention_mask[:, None, :, None].expand_as(bert_output.attentions[i])],
+                bert_output.attentions[i][attention_mask[:, None, :, None].expand_as(bert_output.attentions[i])],
+                atol=1e-4,
+            )
+        assert torch.allclose(tite_output.hidden_states[i], bert_output.hidden_states[i][attention_mask], atol=1e-4)
     assert torch.allclose(
-        tite_output.last_hidden_state[attention_mask], bert_output.last_hidden_state[attention_mask], atol=1e-6
+        tite_output.last_hidden_state[attention_mask], bert_output.last_hidden_state[attention_mask], atol=1e-4
     )
